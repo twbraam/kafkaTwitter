a66f6fc01c291210ac25f4478fb473ef6fe34ffc
Thomas Braam <thomas.braam@gmail.com>
2020-02-05 20:25:18 +0800

    changed gitignore

--- src/main/scala/com/twbraam/kafkatwitter/config/TwitterConfiguration.scala
+++
@@ -1,9 +0,0 @@
-package com.twbraam.kafkatwitter.config
-
-object TwitterConfiguration {
-  val CONSUMER_KEY = "-"
-  val CONSUMER_SECRET = "-"
-  val ACCESS_TOKEN = "-"
-  val TOKEN_SECRET = "-"
-  val HASHTAG = "#bigdata"
-}
