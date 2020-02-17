ad2439faafa01686a614aed6724d2ef2f101fbe3
Thomas Braam <thomas.braam@gmail.com>
2020-02-17 21:34:21 +0800

    Consume as tweet class

--- src/main/scala/com/twbraam/twittercurator/kafkaflink/KafkaFlink.scala
+++ src/main/scala/com/twbraam/twittercurator/kafkaflink/KafkaFlink.scala
@@ -3,7 +3,10 @@ package com.twbraam.twittercurator.kafkaflink
 import java.util.{Properties, UUID}
 
 import com.twbraam.twittercurator.config.KafkaConfiguration
+import com.twbraam.twittercurator.kafkaflink.model.Tweet
+import com.twbraam.twittercurator.twittertimed.producer.TwitterTimedProducer.gson
 import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.datastream.DataStreamSink
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
@@ -14,15 +17,15 @@ import org.apache.flink.streaming.api.scala._
 object KafkaFlink extends App {
   val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
-
-
   val props = new Properties()
   props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConfiguration.SERVERS)
   props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID.toString)
 
-  val stream: DataStreamSink[String] = env
-    .addSource(new FlinkKafkaConsumer[String](KafkaConfiguration.TOPIC_TIMED, new SimpleStringSchema(), props).setStartFromEarliest())
+  val stream: DataStreamSink[Tweet] = env
+    .addSource(new FlinkKafkaConsumer[String](KafkaConfiguration.TOPIC_TIMED, new SimpleStringSchema, props).setStartFromEarliest())
+    .map(gson.fromJson(_, classOf[Tweet]))
     .print()
+
 
   env.execute("Test")
 
